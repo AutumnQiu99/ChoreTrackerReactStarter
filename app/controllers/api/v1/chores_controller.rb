977c7746a5d011f9b469c6331dd15d5c877c2394
AutumnQiu99 <autumn.t.qiu@gmail.com>
2025-04-08 19:27:47 -0400

    index action

--- app/controllers/api/v1/chores_controller.rb
+++ app/controllers/api/v1/chores_controller.rb
@@ -1,5 +1,8 @@
 module Api::V1
   class ChoresController < ApiController
-    
+    def index
+      @chores = Chore.chronological
+      render json: ChoreSerializer.new(@chores).serialized_json
+    end
   end
 end
